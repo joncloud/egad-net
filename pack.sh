3da0c78e38affb16b9d953d66d7bec7c5b8beccd
joncloud <jdberube@gmail.com>
2018-04-17 20:43:44 -0700

    Updates pack step to only pack the src folder.

--- pack.sh
+++ pack.sh
@@ -8,8 +8,8 @@ find ./src -name *.nupkg | xargs -L1 rm
 if [ -z $VERSION_SUFFIX ]; then
   VERSION=$(grep -Po ./src/Egad/Egad.csproj -e '(?<=<VersionPrefix>)[^<]+')
   echo "Using Version $VERSION"
-  dotnet pack --configuration=Release /property:Version=$VERSION
+  dotnet pack --configuration=Release /property:Version=$VERSION ./src/Egad/Egad.csproj
 else
   echo "Using Version Suffix $VALUE"
-  dotnet pack --configuration=Release --version-suffix="$VALUE"
+  dotnet pack --configuration=Release --version-suffix="$VALUE" ./src/Egad/Egad.csproj
 fi
